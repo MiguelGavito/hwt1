37606f43776b99bd5868f96c263d19a76c030c06
MiguelGavito <mgavitogzz@gmail.com>
2025-02-21 20:00:11 -0600

    Tarea completada, no añadi las funcionaldades extras pero cumple con los requisitos y se ve bien

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
+
+interface ImageProps {
+  href: string;
+  src: string;
+  alt: string;
+}
+
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen space-y-4">
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={300}
-        height={100}
-      />
-      <h1 className="text-4xl font-bold text-white">Hello, World!</h1>
-      <p className="text-white">Welcome to your Next.js app.</p>
+      <h1 className="text-4xl font-bold text-white">Tarea 2</h1>
+      <p className="text-white">De: Miguel Ángel Gavito González</p>
+      
+      {/* Contenedor de imágenes en una fila */}
+      <div className="flex space-x-4">
+        <ImageComponent href="/counter" src="/counteri.png" alt="Image 1" />
+        <ImageComponent href="/rectcol" src="/colrecti.png" alt="Image 2" />
+      </div>
     </main>
+  );
+}
+
+function ImageComponent({ href, src, alt }: ImageProps) {
+  return (
+    <div className="w-60 h-40">
+      <Link href={href}>
+        <Image src={src} alt={alt} width={300} height={200} />
+      </Link>
+    </div>
   );
 }
